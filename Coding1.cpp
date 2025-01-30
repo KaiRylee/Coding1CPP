64273409ef79dd1ff3c6e53500dcde037069357d
KaiRylee <emmargooch10@gmail.com>
2025-01-30 12:06:42 -0600

    Reuploading code, troubleshooting
    Last file had a corruption issue, so I just restarted it

---
+++ Coding1.cpp
@@ -0,0 +1,19 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+// No. 2s: in Main() ask user what their name is, then say "Hello <name>!"
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, name);
+    if (name == "Emma" or "Katie") {
+        std::cout << "Hello, creator! <3" << std::endl;
+    }
+    else {
+        std::cout << "Hello, " << name << "! <3" << std::endl;
+    }
+
+}
